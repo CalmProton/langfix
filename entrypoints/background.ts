a9aa75f463bc69f97177086628eddb53c08de879
Denis <denisamania@gmail.com>
2025-12-11 17:54:57 +0200

    feat: add imports for defineBackground and browser in background.ts

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,6 @@
+import { defineBackground } from 'wxt/utils/define-background';
+import { browser } from '#imports';
+
 export default defineBackground(() => {
   console.log('Hello background!', { id: browser.runtime.id });
 });
